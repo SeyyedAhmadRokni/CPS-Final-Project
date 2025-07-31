f06bfe1d5fcfd1aeca1206b372c9572ed323a456
Ali <you@example.com>
2025-07-31 14:53:02 +0330

    Fix master code to use AP3216_WE

--- Arduino/master/master.ino
+++ Arduino/master/master.ino
@@ -2,7 +2,7 @@
 #include <Adafruit_VL53L0X.h>
 #include <DHT.h>
 #include <SoftwareSerial.h>
-#include "AP3216C.h"
+#include <AP3216_WE.h>
 
 // ==== VL53L0X ====
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
@@ -14,7 +14,8 @@ Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 DHT dht(DHTPIN, DHTTYPE);
 
 // ==== AP3216C ====
-AP3216C lightSensor;
+#define AP3216_I2C_ADDR 0x1E  // Default I2C address
+AP3216_WE lightSensor = AP3216_WE(AP3216_I2C_ADDR);
 
 // ==== LED ====
 #define LED_PIN 9
@@ -30,26 +31,29 @@ void setup() {
   dht.begin();
   Wire.begin();
 
+  // Initialize VL53L0X
   if (!lox.begin()) {
     Serial.println("VL53L0X not found!");
     while (1);
   }
 
-  if (!lightSensor.begin()) {
-    Serial.println("AP3216C not found!");
-    while (1);
-  }
+  // Initialize AP3216 with AP3216_WE library
+  lightSensor.init();
+  
+  // Configure sensor settings
+  lightSensor.setLuxRange(RANGE_20661);  // Set to highest range
+  lightSensor.setMode(AP3216_ALS);      // Ambient light sensing mode
 
   Serial.println("Master Ready.");
 }
 
 void loop() {
-  // Distance
+  // Distance measurement
   VL53L0X_RangingMeasurementData_t measure;
   lox.rangingTest(&measure, false);
 
-  // Light
-  float lux = lightSensor.readLux();
+  // Light measurement - changed to getAmbientLight()
+  float lux = lightSensor.getAmbientLight();
 
   // Temp & Humidity
   float temp = dht.readTemperature();
@@ -68,15 +72,15 @@ void loop() {
     ss.println(hum);
 
     if (isNight || hum > 80) {
-      analogWrite(LED_PIN, 255);
+      analogWrite(LED_PIN, 255);  // 100% brightness
     } else {
-      analogWrite(LED_PIN, 0);
+      analogWrite(LED_PIN, 0);    // Off during day
     }
   } else {
     if (isNight || hum > 80) {
-      analogWrite(LED_PIN, 77); // حدود 30٪
+      analogWrite(LED_PIN, 77);   // ~30% brightness
     } else {
-      analogWrite(LED_PIN, 0);
+      analogWrite(LED_PIN, 0);    // Off during day
     }
   }
 
